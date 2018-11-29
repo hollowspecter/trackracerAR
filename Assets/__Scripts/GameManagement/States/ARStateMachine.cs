4a779a2644891b682120a7460c4cba03002eae41
Vivien Baguio <vivien.baguio@rockabyte.com>
2018-11-29 11:24:44 +0100

    Added ARFoundation Samples

--- Assets/__Scripts/GameManagement/States/ARStateMachine.cs
+++ Assets/__Scripts/GameManagement/States/ARStateMachine.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GoogleARCore;
+﻿using UnityEngine;
 
 public interface IARStateMachine
 {
@@ -9,7 +6,7 @@ public interface IARStateMachine
 
 public class ARStateMachine : StateMachine, IARStateMachine
 {
-    private bool m_isQuitting = false;
+    //private bool m_isQuitting = false;
 
     #region State Methods
 
@@ -18,62 +15,62 @@ public class ARStateMachine : StateMachine, IARStateMachine
         base.EnterState ();
     }
 
-    public override void UpdateActive ( double _deltaTime )
-    {
-#if UNITY_ANDROID
-        UpdateApplicationLifecycle ();
-#endif
-        base.UpdateActive ( _deltaTime );
-    }
+    //    public override void UpdateActive ( double _deltaTime )
+    //    {
+    //#if UNITY_ANDROID
+    //        UpdateApplicationLifecycle ();
+    //#endif
+    //    base.UpdateActive ( _deltaTime );
+    //}
 
     #endregion
 
-    private void UpdateApplicationLifecycle ()
-    {
-        // Exit the app when the 'back' button is pressed.
-        if ( Input.GetKey ( KeyCode.Escape ) )
-        {
-            Application.Quit ();
-        }
+    //private void UpdateApplicationLifecycle ()
+    //{
+    //    // Exit the app when the 'back' button is pressed.
+    //    if ( Input.GetKey ( KeyCode.Escape ) )
+    //    {
+    //        Application.Quit ();
+    //    }
 
-        // Only allow the screen to sleep when not tracking.
-        if ( Session.Status != SessionStatus.Tracking )
-        {
-            const int lostTrackingSleepTimeout = 15;
-            Screen.sleepTimeout = lostTrackingSleepTimeout;
-        }
-        else
-        {
-            Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        }
+    //    // Only allow the screen to sleep when not tracking.
+    //    if ( Session.Status != SessionStatus.Tracking )
+    //    {
+    //        const int lostTrackingSleepTimeout = 15;
+    //        Screen.sleepTimeout = lostTrackingSleepTimeout;
+    //    }
+    //    else
+    //    {
+    //        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    //    }
 
-        if ( m_isQuitting )
-        {
-            return;
-        }
+    //    if ( m_isQuitting )
+    //    {
+    //        return;
+    //    }
 
-        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
-        if ( Session.Status == SessionStatus.ErrorPermissionNotGranted )
-        {
-            AnroidUtility.ShowAndroidToastMessage ( "Camera permission is needed to run this application. Please quit the Application." );
-            m_isQuitting = true;
-        }
-        else if ( Session.Status.IsError () )
-        {
-            AnroidUtility.ShowAndroidToastMessage ( "ARCore encountered a problem connecting.  Please start the app again." );
-            m_isQuitting = true;
-        }
+    //    // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+    //    if ( Session.Status == SessionStatus.ErrorPermissionNotGranted )
+    //    {
+    //        AnroidUtility.ShowAndroidToastMessage ( "Camera permission is needed to run this application. Please quit the Application." );
+    //        m_isQuitting = true;
+    //    }
+    //    else if ( Session.Status.IsError () )
+    //    {
+    //        AnroidUtility.ShowAndroidToastMessage ( "ARCore encountered a problem connecting.  Please start the app again." );
+    //        m_isQuitting = true;
+    //    }
 
-    }
+    //}
 
     /// <summary>
     /// Actually quit the application.
     /// </summary>
-    private void _DoQuit ()
-    {
-#if UNITY_EDITOR
-        Debug.LogError ( "Quitting the application does not work in Unity Editor. Please stop PlayMode." );
-#endif
-        Application.Quit ();
-    }
+    //    private void _DoQuit ()
+    //    {
+    //#if UNITY_EDITOR
+    //        Debug.LogError ( "Quitting the application does not work in Unity Editor. Please stop PlayMode." );
+    //#endif
+    //    Application.Quit ();
+    //}
 }
