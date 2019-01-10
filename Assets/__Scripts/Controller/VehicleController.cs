a59982d72e4b6c334cea3fa8444988e4a092f2cd
Vivien Baguio <baguiov@posteo.net>
2019-01-10 13:57:56 +0100

    Added Car following a Spline

---
+++ Assets/__Scripts/Controller/VehicleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    private const float WAYPOINT_RADIUS = 0.3f;
+
+    public SplineManager m_splineManager;
+    public float speed = 1f;
+
+    private List<OrientedPoint> m_waypoints;
+    private int current = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_waypoints = m_splineManager.m_waypoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, m_waypoints[current].position) <= WAYPOINT_RADIUS/2f)
+        {
+            current = ( current + 1 ) % m_waypoints.Count;
+        }
+
+        transform.position = Vector3.MoveTowards ( transform.position, m_waypoints [ current ].position, speed * Time.deltaTime );
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere ( transform.position, WAYPOINT_RADIUS );
+    }
+}
--- Assets/__Scripts/Painter/SplineManager.cs
+++ Assets/__Scripts/Painter/SplineManager.cs
@@ -6,20 +6,31 @@ using UnityEngine;
 public class SplineManager : UniqueMesh
 {
     [SerializeField]
-    private ShapeData m_shape;
-    [SerializeField]
-    private Vector2 m_scale;
+    private TrackData m_trackData;
+
+    public List<OrientedPoint> m_waypoints;
 
     private Vector3 [] m_points;
+    private OrientedPoint [] m_path;
     private Spline m_spline;
 
-    void Start ()
+    #region Unity Functions
+
+    private void Awake()
     {
         InitSpline ();
         GenerateStreet ();
     }
 
-    void InitSpline ()
+    #endregion
+
+    #region Public Functions
+
+    #endregion
+
+    #region Private Functions
+
+    private void InitSpline()
     {
         m_points = new Vector3 [ transform.childCount ];
         for ( int i = 0; i < m_points.Length; ++i )
@@ -29,12 +40,28 @@ public class SplineManager : UniqueMesh
         m_spline = new Spline ( m_points );
     }
 
-    void GenerateStreet ()
+    private void GenerateStreet()
     {
-        OrientedPoint [] path;
-        m_spline.GetOrientedPath ( out path );
-        Extruder.Extrude ( mesh, m_shape, path, m_scale );
+        m_spline.GetOrientedPath ( out m_path );
+        GenerateWaypoints ();
+        Extruder.Extrude ( mesh, m_trackData.m_shape, m_path, m_trackData.m_scale );
     }
+
+    private void GenerateWaypoints()
+    {
+        if ( m_path == null || m_path.Length == 0 ) return;
+        m_waypoints = new List<OrientedPoint> ();
+
+        // Todo: hardcoding rausnehmen
+        for (int i=0; i<m_path.Length; i+=8 )
+        {
+            m_waypoints.Add ( m_path [ i ] );
+        }
+    }
+
+    #endregion
+
+    #region Debug Functions
 
     private void OnDrawGizmos ()
     {
@@ -52,6 +79,17 @@ public class SplineManager : UniqueMesh
         m_spline.DrawGizmos ();
 
         // Draw Shape
-        if ( m_shape != null ) m_shape.DrawGizmos ( transform.position );
+        if ( m_trackData.m_shape != null ) m_trackData.m_shape.DrawGizmos ( transform.position );
+
+        // Draw WayPoints
+        if (m_waypoints != null)
+        {
+            foreach(OrientedPoint p in m_waypoints)
+            {
+                Gizmos.DrawSphere ( p.position, 0.1f );
+            }
+        }
     }
+
+    #endregion
 }
