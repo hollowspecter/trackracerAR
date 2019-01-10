eb6a191126f40379d92662d75b0bc3c2726605d0
Vivien Baguio <baguiov@posteo.net>
2019-01-10 11:30:15 +0100

    Implemented Closed Catmull Rom Splines

--- Assets/__Scripts/Painter/Spline.cs
+++ Assets/__Scripts/Painter/Spline.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 
 public class Spline
 {
+    private const float THRESHOLD = 1f;
+
     private Vector3 [] m_points;
     private int m_pointsPerCurve;
     private Curve [] m_curves;
 
     #region Constructor
 
-    public Spline ( Vector3 [] _points, int _precision = 5 )
+    public Spline ( Vector3 [] _points, bool closed = true, int _precision = 5 )
     {
         // check for traps
         if ( _points == null ) throw new System.ArgumentNullException ( "_points" );
@@ -25,14 +27,15 @@ public class Spline
             m_points [ i ] = _points [ i ];
         }
 
-        m_curves = new Curve [ m_points.Length - 1 ];
-        CalculateSpline ();
+        if (closed) CalculateClosedSpline ();
+        else CalculateSpline ();
     }
 
     #endregion
 
     private void CalculateSpline ()
     {
+        m_curves = new Curve [ m_points.Length - 1 ];
         Vector3 p0, p1, p2, p3; // line will be drawn between p1 and p2
         for ( int i = 0; i < m_points.Length - 1; ++i )
         {
@@ -50,6 +53,42 @@ public class Spline
                 p3 = p2 + ( p2 - p1 );
             else
                 p3 = m_points [ i + 2 ];
+
+            // generate curve
+            m_curves [ i ] = Curve.CatmulRom ( p0, p1, p2, p3, m_pointsPerCurve );
+        }
+    }
+
+    private void CalculateClosedSpline()
+    {
+        // check if last and first point are on the same position
+        bool skipLastPoint = ( ( m_points [ 0 ] - m_points [ m_points.Length - 1 ] ).magnitude < THRESHOLD );
+        Vector3 [] points = m_points;
+        if (skipLastPoint)
+        {
+            points = new Vector3 [ m_points.Length - 1 ];
+            for ( int i = 0; i < points.Length; ++i ) points [ i ] = m_points [ i ];
+        }
+        m_curves = new Curve [ points.Length ];
+
+        // generate spline
+        Vector3 p0, p1, p2, p3;
+        for ( int i = 0; i < points.Length; ++i )
+        {
+            p1 = points [ i ];
+            p2 = points [ (i + 1)%points.Length];
+
+            // first curve exception
+            if ( i == 0 )
+                p0 = points [ points.Length - 1 ];
+            else
+                p0 = points [ ( i - 1 ) % points.Length ];
+
+            // last curve exception
+            if ( i == points.Length - 2 )
+                p3 = points [ 0 ];
+            else
+                p3 = points [ ( i + 2 ) % points.Length ];
 
             // generate curve
             m_curves [ i ] = Curve.CatmulRom ( p0, p1, p2, p3, m_pointsPerCurve );
