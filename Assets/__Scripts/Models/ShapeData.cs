1cfba37fc729f65eb90946afbffc3e682d168bf2
Vivien Baguio <baguiov@posteo.net>
2019-01-10 12:24:32 +0100

    TrackData can be saved and loaded (untested)

---
+++ Assets/__Scripts/Models/ShapeData.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu ( menuName = "Custom/ShapeData" )]
+public class ShapeData : ScriptableObject
+{
+    public Vector2 [] m_verts;
+    public Vector2 [] m_normals;
+    public float [] m_us;
+
+    public void DrawGizmos ( Vector3 _pivot )
+    {
+        // normals
+        Gizmos.color = Color.green;
+        if ( m_verts.Length == m_normals.Length )
+        {
+            for ( int i = 0; i < m_normals.Length; ++i )
+            {
+                Vector3 tmp = _pivot + new Vector3 ( m_verts [ i ].x, m_verts [ i ].y, 0 );
+                Gizmos.DrawLine ( tmp, tmp + new Vector3 ( m_normals [ i ].x, m_normals [ i ].y, 0 ) );
+            }
+        }
+
+        // line
+        Gizmos.color = Color.red;
+        if ( m_verts.Length > 0 )
+        {
+            for ( int i = 0; i < m_verts.Length - 1; ++i )
+            {
+                Gizmos.DrawLine ( _pivot + new Vector3 ( m_verts [ i ].x, m_verts [ i ].y, 0 ), _pivot + new Vector3 ( m_verts [ i + 1 ].x, m_verts [ i + 1 ].y, 0 ) );
+            }
+        }
+    }
+}
---
+++ Assets/__Scripts/Models/TrackData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+// Todo: Test this shite!
+[CreateAssetMenu(menuName ="Custom/TrackData")]
+public class TrackData : ScriptableObject
+{
+    private const string FOLDER= "/Tracks/";
+    private const string FILE_ENDING = ".track";
+
+    public Vector2 m_scale = Vector2.one;
+    [Range(1,10)]
+    public int precision = 5;
+    public bool closed = true;
+    public ShapeData m_shape;
+
+    public static string GetPath()
+    {
+#if UNITY_EDITOR
+        return Application.dataPath + "/StreamingAssets" + FOLDER;
+#else
+        return Application.persistentDataPath + FOLDER;
+#endif
+    }
+
+    public static void SaveTrack(TrackData _track, string _name )
+    {
+        string json = JsonUtility.ToJson ( _track );
+        File.WriteAllText ( GetPath () + _name + FILE_ENDING, json );
+#if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh ();
+#endif
+    }
+
+    public static bool LoadTrack(string _name, out TrackData _track)
+    {
+        // try load the track
+        string filename = GetPath () + _name + FILE_ENDING;
+        if (File.Exists(filename))
+        {
+            try
+            {
+                string json = File.ReadAllText ( filename );
+                _track = JsonUtility.FromJson<TrackData> ( json );
+                return true;
+            } catch (Exception)
+            {
+                Debug.LogWarning ( "There went something wrong while loading the Track" );
+            }
+        }
+
+        // fallback
+        _track = null;
+        return false;
+    }
+}
