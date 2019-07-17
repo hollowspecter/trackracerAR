e2e51d9f4b2af91538b6eb041ed7ca4f7d0c277e
Vivien Baguio <baguiov@posteo.net>
2019-07-17 22:18:09 +0200

    Add calibration progress bar

---
+++ Assets/__Scripts/GameManagement/States/CalibrateState.cs
@@ -0,0 +1,51 @@
+﻿/* Copyright 2019 Vivien Baguio.
+ * Subject to the GNU General Public License.
+ * See https://www.gnu.org/licenses/gpl.txt
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARExtensions;
+using Zenject;
+
+public interface ICalibrateState
+{
+    void DoStart();
+    void Restart();
+}
+
+public class CalibrateState : State, ICalibrateState
+{
+    private ARSession m_arSession;
+
+    #region DI
+
+    [Inject]
+    private void Construct( ARSession _arSession )
+    {
+        m_arSession = _arSession;
+    }
+
+    #endregion
+
+    public override void EnterState()
+    {
+        base.EnterState ();
+        Debug.Log ("CalibrateState entered!");
+        m_arSession.Reset ();
+    }
+
+    public void DoStart()
+    {
+        if ( !Active ) return;
+        m_stateMachine.TransitionToState (StateName.AR_SM);
+    }
+
+    public void Restart()
+    {
+        if ( !Active ) return;
+        EnterState ();
+    }
+}
