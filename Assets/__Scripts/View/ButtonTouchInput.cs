cededc7eecddc23c980c33f4f533946e238cfaef
Vivien Baguio <baguiov@posteo.net>
2019-07-18 16:31:02 +0200

    fix paint state

---
+++ Assets/__Scripts/View/ButtonTouchInput.cs
@@ -0,0 +1,29 @@
+﻿/* Copyright 2019 Vivien Baguio.
+ * Subject to the GNU General Public License.
+ * See https://www.gnu.org/licenses/gpl.txt
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Zenject;
+
+public class ButtonTouchInput : Button
+{
+    IBuildPaintState m_state;
+
+    [Inject]
+    public void Construct(IBuildPaintState _state)
+    {
+        m_state = _state;
+    }
+
+    private void Update()
+    {
+        if (this.currentSelectionState == SelectionState.Pressed) {
+            m_state.OnTouchDetected (Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+}
