d29474853222f6c5a72ff837d327ca1cfb56b1b6
Vivien Baguio <baguiov@posteo.net>
2019-07-18 18:04:52 +0200

    Fixes and UX Snackbar improvments

---
+++ Assets/__Scripts/Controller/CenterTrackTool.cs
@@ -0,0 +1,13 @@
+﻿/* Copyright 2019 Vivien Baguio.
+ * Subject to the GNU General Public License.
+ * See https://www.gnu.org/licenses/gpl.txt
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterTrackTool : MonoBehaviour
+{
+    
+}
