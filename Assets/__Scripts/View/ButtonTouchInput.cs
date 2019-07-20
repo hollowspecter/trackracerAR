c4268005b158cfc24aa3365187655989fcf59a24
Vivien Baguio <baguiov@posteo.net>
2019-07-20 12:36:03 +0200

    Tap and hold animation for Paint State

--- Assets/__Scripts/View/ButtonTouchInput.cs
+++ Assets/__Scripts/View/ButtonTouchInput.cs
@@ -9,21 +9,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Zenject;
+using UniRx;
 
 public class ButtonTouchInput : Button
 {
+    public ReactiveProperty<bool> IsPressedx { get; private set; }
+
     IBuildPaintState m_state;
 
     [Inject]
     public void Construct(IBuildPaintState _state)
     {
         m_state = _state;
+        IsPressedx = new ReactiveProperty<bool> (false);
     }
 
     private void Update()
     {
-        if (this.currentSelectionState == SelectionState.Pressed) {
-            m_state.OnTouchDetected (Input.mousePosition.x, Input.mousePosition.y);
+        if (IsPressedx != null) { // update method gets also called in editor moce
+            IsPressedx.Value = (currentSelectionState == SelectionState.Pressed);
+            if ( IsPressedx.Value ) {
+                m_state.OnTouchDetected (Input.mousePosition.x, Input.mousePosition.y);
+            }
         }
     }
 }
---
+++ Assets/__Scripts/View/PaintHelpView.cs
@@ -0,0 +1,59 @@
+﻿/* Copyright 2019 Vivien Baguio.
+ * Subject to the GNU General Public License.
+ * See https://www.gnu.org/licenses/gpl.txt
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UniRx;
+
+public class PaintHelpView : MonoBehaviour
+{
+    public Animator m_animator;
+    public ButtonTouchInput m_buttonTouchInput;
+    public Graphic [] m_graphics;
+
+    private IDisposable m_subscription;
+    private Sequence m_sequence;
+
+    private void Awake()
+    {
+        m_sequence = DOTween.Sequence ();
+        m_sequence.SetAutoKill (false);
+        for ( int i = 0; i < m_graphics.Length; ++i ) {
+            m_sequence.Join (m_graphics [i].DOFade (0f, 1f))
+                .Pause ()
+                .OnComplete (() => m_animator.SetTrigger ("Stop"));
+        }
+    }
+
+    private void FadeOut()
+    {
+        m_subscription?.Dispose ();
+        if (!m_sequence.IsPlaying()) {
+            m_sequence.PlayForward ();
+        }
+    }
+
+    private void OnEnable()
+    {
+        for ( int i = 0; i < m_graphics.Length; ++i ) {
+            m_graphics [i].color = Color.white;
+        }
+        m_animator.SetTrigger ("Start");
+        m_sequence.Rewind ();
+        m_subscription = m_buttonTouchInput.IsPressedx
+            .Where(isPressed => isPressed)
+            .Delay(TimeSpan.FromMilliseconds(250))
+            .Subscribe (_ => FadeOut ());
+    }
+
+    private void OnDisable()
+    {
+        m_subscription?.Dispose ();
+    }
+}
